d9926d86fb024b78521fe06a08af0a36ca84af11
Gulshid Zada <gulshidzada4@gmail.com>
2026-07-27 16:52:06 +0500

    Todo APp completed successfully

---
+++ Todo app/Todo app/Todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_appApp.swift
+//  Todo app
+//
+//  Created by Gulshid Zada on 27/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Todo app/Todo appTests/Todo_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_appTests.swift
+//  Todo appTests
+//
+//  Created by Gulshid Zada on 27/07/2026.
+//
+
+import Testing
+@testable import Todo_app
+
+struct Todo_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
